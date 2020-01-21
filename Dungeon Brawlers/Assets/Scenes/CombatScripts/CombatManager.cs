7d4ad565739370dc7171ffeba66e1e7e2d6fb217
Hamel103 <hamel103@mail.chapman.edu>
2020-01-21 01:56:25 -0800

    Mechanics Required for Combat
    Implemented a few of the mechanics necessary for combat, will begin work on the combat screen and actions tomorrow. This commit mainly deals with the requirements to initiate combat.

---
+++ Dungeon Brawlers/Assets/Scenes/CombatScripts/CombatManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    private bool inCombat;
+    private int firstStrike;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inCombat = false;
+        firstStrike = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetInCombat()
+    {
+        return inCombat;
+    }
+
+    public void SetInCombat(bool newValue)
+    {
+        inCombat = newValue;
+    }
+
+    public int GetFirstStrike()
+    {
+        return firstStrike;
+    }
+
+    public void SetFirstStrike(int newValue)
+    {
+        firstStrike = newValue;
+    }
+}
---
+++ Dungeon Brawlers/Assets/Scenes/CombatScripts/Temporary Scripts/PlayerTwoMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoMove : MonoBehaviour
+{
+    public float speed;
+    private bool inRange;
+    private GameObject cm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm = GameObject.FindGameObjectWithTag("CombatManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!cm.GetComponent<CombatManager>().GetInCombat())
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!cm.GetComponent<CombatManager>().GetInCombat() && collision.tag == "Player" && Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("The Lesser Player Intiates Combat");
+            /* UNCOMMENT AFTER TESTING */
+            //cm.GetComponent<CombatManager>().SetInCombat(true);
+            cm.GetComponent<CombatManager>().SetFirstStrike(2);
+        }
+    }
+}
